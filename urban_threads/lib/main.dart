f40a113eba1099c17446ca4ee2b81a0f6c84ae83
shenalchanu8 <125878126+shenalchanu8@users.noreply.github.com>
2026-06-26 09:29:04 +0530

    update Main

---
+++ urban_threads/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/cart_provider.dart';
+import 'providers/favorite_provider.dart';
+import 'providers/product_provider.dart';
+import 'providers/theme_provider.dart';
+import 'providers/auth_provider.dart';
+import 'screens/splash/onboarding_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/Auth/register_screen.dart';
+import 'screens/main/main_screen.dart';
+
+void main() {
+  runApp(const UrbanThreadsApp());
+}
+
+class UrbanThreadsApp extends StatelessWidget {
+  const UrbanThreadsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+        ChangeNotifierProvider(
+            create: (_) => ProductProvider()..loadProducts()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            title: 'Urban Threads',
+            theme: themeProvider.theme,
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const OnboardingScreen(),
+              '/login': (context) => const LoginScreen(),
+              '/register': (context) => const RegisterScreen(),
+              '/home': (context) => const MainScreen(),
+            },
+          );
+        },
+      ),
+    );
+  }
+}
